54a15d95a59535ca35c2463f151e91b4218f3270
benjaAguilar <benjaaguilar02@gmail.com>
2024-03-21 15:32:12 -0300

    created the main template

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'html',
+        template: './src/index.html',
+      }),
+    ],
+  },
+};
